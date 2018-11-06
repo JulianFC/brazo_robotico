d6c23e608a6cd78a827d6a5dde428053bea33736
Julian <julian.fuchslocher@ug.uchile.cl>
2018-11-06 00:04:00 -0300

    added arm_animation

--- main.py
+++ main.py
@@ -3,26 +3,30 @@ from deap import creator, base, tools, algorithms
 from math import pi
 from denavit_hartenberg import direct_problem
 from arm_animation import *
+
+
+
 creator.create("FitnessMax", base.Fitness, weights=(1.0,))
 creator.create("Individual", list, fitness=creator.FitnessMax)
 
 toolbox = base.Toolbox()
 
-toolbox.register("attr_bool", random.uniform, -2*pi, 2*pi)
+toolbox.register("attr_bool", random.uniform, -pi, pi)
 toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_bool, n=4)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
 def evalOneMax(individual, obj):
     QT, P= direct_problem(individual, 4)
-    #print(P)
     diff = 0
     for (coord, obj_coord) in zip(P, obj):
         diff = diff+abs(coord-obj_coord)
     return 1/diff,
 
-toolbox.register("evaluate", evalOneMax, obj = [0, 1, 1])
-toolbox.register("mate", tools.cxBlend,alpha = 1)
-toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
+objective = [random.uniform(-2,2),random.uniform(-2,2),random.uniform(0,3)]
+
+toolbox.register("evaluate", evalOneMax, obj=objective)
+toolbox.register("mate", tools.cxUniform, indpb=0.25)
+toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=1, indpb=0.05)
 toolbox.register("select", tools.selTournament, tournsize=3)
 
 population = toolbox.population(n=300)
@@ -39,6 +43,9 @@ for gen in range(NGEN):
     #    top = tools.selBest(population, k=1)[0]
     #    animate([0, 0, 0, 0], top)
 top10 = tools.selBest(population, k=10)
-
-animate([0, 0, 0, 0], [pi/2,0,0,0])
-print(top10)
+top = tools.selBest(population, k=1)[0]
+animate([random.uniform(-pi, pi), random.uniform(-pi, pi), random.uniform(-pi, pi), random.uniform(-pi, pi)], top)
+#print(top10)
+QT, P = direct_problem(top, 4)
+print(P)
+print(objective)
