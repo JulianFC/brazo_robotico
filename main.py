a493a47d8b88d73ed2672f8366d6b887b2bfd49a
Julian <julian.fuchslocher@ug.uchile.cl>
2018-11-09 21:44:04 -0300

    modified main to get 10 results and plot

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from math import pi
 from denavit_hartenberg import direct_problem
 from arm_animation import *
 
-
-
 creator.create("FitnessMax", base.Fitness, weights=(1.0,))
 creator.create("Individual", list, fitness=creator.FitnessMax)
 
@@ -15,37 +13,66 @@ toolbox.register("attr_bool", random.uniform, -pi, pi)
 toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_bool, n=4)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
+
 def evalOneMax(individual, obj):
-    QT, P= direct_problem(individual, 4)
+    QT, P = direct_problem(individual, 4)
     diff = 0
     for (coord, obj_coord) in zip(P, obj):
-        diff = diff+abs(coord-obj_coord)
-    return 1/diff,
+        diff = diff + abs(coord - obj_coord)
+    return 1 / (1+diff),
 
-objective = [random.uniform(-2,2),random.uniform(-2,2),random.uniform(0,3)]
 
-toolbox.register("evaluate", evalOneMax, obj=objective)
-toolbox.register("mate", tools.cxUniform, indpb=0.25)
-toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=1, indpb=0.05)
-toolbox.register("select", tools.selTournament, tournsize=3)
+objectives = []
+results = []
+i = 0
+fitness_evolution = []
+X = []
+while i < 10:
+    i = i+1
 
-population = toolbox.population(n=300)
 
-NGEN=100
-for gen in range(NGEN):
-    offspring = algorithms.varAnd(population, toolbox, cxpb=0.5, mutpb=0.1)
-    fits = toolbox.map(toolbox.evaluate, offspring)
-    for fit, ind in zip(fits, offspring):
-        ind.fitness.values = fit
-    population = toolbox.select(offspring, k=len(population))
 
-    #if gen%20 == 0:
-    #    top = tools.selBest(population, k=1)[0]
-    #    animate([0, 0, 0, 0], top)
-top10 = tools.selBest(population, k=10)
-top = tools.selBest(population, k=1)[0]
-animate([random.uniform(-pi, pi), random.uniform(-pi, pi), random.uniform(-pi, pi), random.uniform(-pi, pi)], top)
-#print(top10)
-QT, P = direct_problem(top, 4)
-print(P)
-print(objective)
+    objective = [random.uniform(-2,2),random.uniform(-2,2),random.uniform(0,3)]
+
+    toolbox.register("evaluate", evalOneMax, obj=objective)
+    toolbox.register("mate", tools.cxUniform, indpb=0.25)
+    toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=1, indpb=0.1)
+    toolbox.register("select", tools.selTournament, tournsize=3)
+
+    population = toolbox.population(n=300)
+
+    NGEN=100
+    for gen in range(NGEN):
+        offspring = algorithms.varAnd(population, toolbox, cxpb=0.5, mutpb=0.1)
+        fits = toolbox.map(toolbox.evaluate, offspring)
+        for fit, ind in zip(fits, offspring):
+            ind.fitness.values = fit
+        population = toolbox.select(offspring, k=len(population))
+
+        top = tools.selBest(population, k=1)[0]
+        if (i == 9):
+            fitness_evolution.append(evalOneMax(top, objective))
+            X.append(gen)
+
+    #top10 = tools.selBest(population, k=10)
+
+    #animate([random.uniform(-pi, pi), random.uniform(-pi, pi), random.uniform(-pi, pi), random.uniform(-pi, pi)], top)
+    #print(top10)
+    QT, P = direct_problem(top, 4)
+    objectives.append(objective)
+    results.append(P)
+    #print(P)
+    #print(objective)
+
+print("Objetivo:\t\t\t\t\t\tResultado:\t\t\t\t\t\tError:\n")
+for i in range(10):
+    objectives[i] = [round(objectives[i][j],4) for j in range(3)]
+    results[i] = [round(results[i][j],4) for j in range(3)]
+    error = [round(abs(results[i][j]-objectives[i][j]),4) for j in range(3)]
+    print(str(objectives[i])+"\t"+str(results[i])+"\t"+str(error)+"\n")
+
+plt.plot(X,fitness_evolution)
+plt.xlabel("Generación")
+plt.ylabel("Fitness")
+plt.title("Evolución del fitness en el tiempo")
+plt.show()
